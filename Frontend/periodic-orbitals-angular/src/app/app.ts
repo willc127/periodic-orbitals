6aad310e326f40aa89c51e332a675f990f7a9121
William Cesar <williamcesar.ribeiro@outlook.com>
2025-08-21 20:25:08 -0300

    Criação do componente atom-animation

--- Frontend/periodic-orbitals-angular/src/app/app.ts
+++ Frontend/periodic-orbitals-angular/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { PeriodicTable } from './components/periodic-table/periodic-table';
+import { AtomAnimation } from './components/atom-animation/atom-animation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, PeriodicTable],
+  imports: [RouterOutlet, Header, Footer, PeriodicTable,AtomAnimation],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
