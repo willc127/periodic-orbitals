0858eafb6987a8d5151903ca69913bbaff05718a
William Cesar <williamcesar.ribeiro@outlook.com>
2025-10-19 20:54:27 -0300

    Criação de cliente html básico para consumir a API e mostrar a imagem no navegador

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import base64
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "API FastAPI funcionando"}
 
-@app.get("/api/dados")
-async def get_dados():
-    return {"nome": "William", "profissao": "Desenvolvedor"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # aceita qualquer origem (para teste)
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    encoded = base64.b64encode(contents).decode("utf-8")
+    return JSONResponse(
+        {
+            "filename": file.filename,
+            "content_type": file.content_type,
+            "image_base64": encoded,
+        }
+    )
