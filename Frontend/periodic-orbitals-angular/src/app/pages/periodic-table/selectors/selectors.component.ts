47ffc8284d529e50048c62ecb66b162ae6cfea80
William Cesar <williamcesar.ribeiro@outlook.com>
2026-05-23 20:01:38 -0300

    finaliza estilos dos seletores por tipo de elemento

--- Frontend/periodic-orbitals-angular/src/app/pages/periodic-table/selectors/selectors.component.ts
+++ Frontend/periodic-orbitals-angular/src/app/pages/periodic-table/selectors/selectors.component.ts
@@ -7,23 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './selectors.component.scss',
 })
 export class SelectorsComponent {
-  listaEsquerda!: { tipo: string; cor: string }[];
-  listaDireita!: { tipo: string; cor: string }[];
 
-  constructor() {
-    this.listaEsquerda = [
-      {tipo: 'Metal alcalino', cor: '#FF8439'},
-      {tipo: 'Metal alcalino-terroso', cor: '#FFD400'},
-      {tipo: 'Metal de transição', cor: '#354782'},
-      {tipo: 'Lantanídeos', cor: '#AA6B30'},
-      {tipo: 'Actinídeos', cor: '#ACFF8B'},
-    ];
-    this.listaDireita = [
-      {tipo: 'Semimetal', cor: '#1F7063'},
-      {tipo: 'Metal de pós-transição', cor: '#3DDED6'},
-      {tipo: 'Não-metal', cor: '#888529'},
-      {tipo: 'Halogênio', cor: '#6D3C9F'},
-      {tipo: 'Gás nobre', cor: '#FF00AA'},
-    ];
-  }
+  listaEsquerda = [
+    { tipo: 'Metal alcalino', cor: '#FF8439' },
+    { tipo: 'Metal alcalino-terroso', cor: '#FFD400' },
+    { tipo: 'Metal de transição', cor: '#354782' },
+    { tipo: 'Lantanídeos', cor: '#AA6B30' },
+    { tipo: 'Actinídeos', cor: '#ACFF8B' },
+  ];
+  
+  listaDireita = [
+    { tipo: 'Semimetal', cor: '#1F7063' },
+    { tipo: 'Metal de pós-transição', cor: '#3DDED6' },
+    { tipo: 'Não-metal', cor: '#888529' },
+    { tipo: 'Halogênio', cor: '#6D3C9F' },
+    { tipo: 'Gás nobre', cor: '#FF00AA' },
+  ];
 }
