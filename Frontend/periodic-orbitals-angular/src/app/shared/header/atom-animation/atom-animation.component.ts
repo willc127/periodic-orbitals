99bbdeaf823f20a385b592ba3812dcd728cc1108
William Cesar <williamcesar.ribeiro@outlook.com>
2026-05-23 21:45:02 -0300

    refatora animação do átomo no header e adiciona campo de busca de termos

--- Frontend/periodic-orbitals-angular/src/app/shared/header/atom-animation/atom-animation.component.ts
+++ Frontend/periodic-orbitals-angular/src/app/shared/header/atom-animation/atom-animation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-atom-animation',
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './atom-animation.html',
   styleUrl: './atom-animation.scss',
 })
-export class AtomAnimation {}
+export class AtomAnimation {
+  eletrons: number = 10;
+
+  get eletronsList() {
+    return Array.from({ length: this.eletrons }, (_, i) => i + 1);
+  }
+
+  @HostBinding('style.--total-eletrons') get totalEletrons() {
+    return this.eletrons;
+  }
+
+  coresOrbitas = [
+    '#FF8439',
+    '#FFD400',
+    '#354782',
+    '#AA6B30',
+    '#ACFF8B',
+    '#1F7063',
+    '#3DDED6',
+    '#888529',
+    '#6D3C9F',
+    '#FF00AA'
+  ];
+
+  
+}
--- Frontend/periodic-orbitals-angular/src/app/shared/header/header.component.ts
+++ Frontend/periodic-orbitals-angular/src/app/shared/header/header.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AtomAnimation } from './atom-animation/atom-animation.component';
 
 @Component({
   selector: 'app-header',
-  imports: [AtomAnimation],
+  imports: [
+    AtomAnimation,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+}
