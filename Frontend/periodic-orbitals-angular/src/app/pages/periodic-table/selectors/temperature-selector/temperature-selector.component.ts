3279b6208266cccb3cf7d3c008dd688e9fc63744
William Cesar <williamcesar.ribeiro@outlook.com>
2026-07-08 21:25:23 -0300

    Arruma estilos

--- Frontend/periodic-orbitals-angular/src/app/pages/periodic-table/selectors/temperature-selector/temperature-selector.component.ts
+++ Frontend/periodic-orbitals-angular/src/app/pages/periodic-table/selectors/temperature-selector/temperature-selector.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, signal } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 @Component({
   selector: 'app-temperature-selector',
-  imports: [],
+  imports: [MatSliderModule],
   templateUrl: './temperature-selector.html',
-  styleUrl: './temperature-selector.scss'
+  styleUrl: './temperature-selector.scss',
 })
 export class TemperatureSelector {
+  temperaturaKelvin = signal(298); // Kelvin
 
+  temperaturaCelsius = computed(() =>
+    Math.round(this.temperaturaKelvin() - 273.15),
+  );
+  
+  temperaturaFarenheit = computed(() =>
+    Math.round((this.temperaturaCelsius() * 9) / 5 + 32),
+  );
 }
