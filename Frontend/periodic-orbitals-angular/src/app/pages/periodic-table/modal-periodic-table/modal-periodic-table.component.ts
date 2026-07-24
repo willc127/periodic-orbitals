29cb007487dca88dc83c7ef644060ae04fa509bf
William Cesar <williamcesar.ribeiro@outlook.com>
2026-07-23 21:35:25 -0300

    inclui botão de fechar modal

--- Frontend/periodic-orbitals-angular/src/app/pages/periodic-table/modal-periodic-table/modal-periodic-table.component.ts
+++ Frontend/periodic-orbitals-angular/src/app/pages/periodic-table/modal-periodic-table/modal-periodic-table.component.ts
@@ -1,17 +1,18 @@
 import { Component, Inject, signal } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { ButtonToggleModal } from '../../../shared/button-toggle-modal/button-toggle-modal.component';
 import { PropertiesElements } from './properties-elements/properties-elements.component';
 import { EmissionSpectrumComponent } from './emission-spectrum/emission-spectrum.component';
 import { Description } from './description/description.component';
 import { QuantumOrbitalViewerComponent } from "./quantum-orbital-viewer/quantum-orbital-viewer.component";
 import { DiagramaConfiguracaoEletronicaComponent } from "./diagrama-configuracao-eletronica/diagrama-configuracao-eletronica.component";
+import { MatIcon } from "@angular/material/icon";
 @Component({
   selector: 'app-modal-periodic-table',
   standalone: true,
   templateUrl: './modal-periodic-table.html',
   styleUrls: ['./modal-periodic-table.scss'],
-  imports: [ButtonToggleModal, PropertiesElements, EmissionSpectrumComponent, Description, QuantumOrbitalViewerComponent, DiagramaConfiguracaoEletronicaComponent],
+  imports: [ButtonToggleModal, PropertiesElements, EmissionSpectrumComponent, Description, QuantumOrbitalViewerComponent, DiagramaConfiguracaoEletronicaComponent, MatIcon, MatDialogModule],
 })
 export class ModalPeriodicTable {
   selectedView = signal('propriedades');
